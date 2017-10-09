7bff4b8d3b140941500711f3c7e87ce499bb9cef
Arvid E. Picciani <aep@exys.org>
2017-10-09 12:09:01 +0200

    plt testcase

---
+++ tests/samples/plt_main.asm
@@ -0,0 +1,15 @@
+extern hello;
+
+section .text
+    global _start
+_start:
+    push    rbp
+    call    hello wrt ..plt
+    pop	    rbp
+    mov     rax, 60
+    mov     rdi, 10
+    syscall
+    global _useless:
+
+_useless:
+    nop
